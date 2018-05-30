a53a6333fe2594b7fb03e27cd839d685741313fa
Robin Zhang <robinz672@gmail.com>
2018-05-30 00:44:46 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# block-reddit-extension
+
+An chrome extension I scrapped together after wasting an evening browsing
+reddit.
---
+++ block.js
@@ -0,0 +1,11 @@
+document.open();
+let blockPage =
+`
+<html>
+<body>
+  <h1>Stop Wasting Time</h1>
+</body>
+</html>
+`
+document.write(blockPage);
+document.close();
